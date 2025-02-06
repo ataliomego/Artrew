bd458805103b8cba3998bef2be99544de642b2f4
Kyelva <162899465+ataliomego@users.noreply.github.com>
2025-02-07 00:22:39 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import os
 from app import create_app
 
+# Membuat aplikasi dengan memanggil fungsi create_app() dari package app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Menjalankan aplikasi Flask pada host 0.0.0.0 dan port yang didapatkan dari variabel lingkungan PORT
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
